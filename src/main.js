33165164c0f1ef6398153b26dc288aca5f200b1e
chenlei6 <itta163@163.com>
2020-01-21 11:57:08 +0800

    使用element-ui框架，按需引入
    教程：https://juejin.im/post/5d36c8f6e51d454fd8057c5c

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routes/router'
 import store from './store'
+import './components/element-ui'
 
 Vue.config.productionTip = false
 
